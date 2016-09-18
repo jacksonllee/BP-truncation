bf75c4d65bde3c5ff2758e9b0724ae0a6c525053
JacksonLLee <jsllee.phon@gmail.com>
2016-09-17 22:02:57 -0500

    Major overhaul
    Standardize dataset filenames (e.g., use "-tech" instead of "-notech")
    Revamp output files -- now in "plots_for_words/" and "results/"
    main.py:
    - add optional command line arguments for rscript, latex, token freq
    - token freq: use log(token_freq) instead of 1 as count factor
    - evaluation: output in a separate txt all stats and test results
    - boxplot: use boxplot instead of density curves for error distributions
    Update readme.me accordingly

--- main.py
+++ main.py
@@ -3,12 +3,15 @@
 
 from __future__ import print_function
 import os
-import math
+from math import (sqrt, log)
 import subprocess
-import numpy as np
-import scipy.stats as stats
 import multiprocessing as mp
 import argparse
+
+import numpy as np
+import scipy.stats as stats
+import pandas as pd
+import seaborn as sns
 
 
 def elbow_point(points):
@@ -52,7 +55,7 @@ def sum_abs(number_list, plus=0):
 
 def rms(number_list):
     """root mean square"""
-    return math.sqrt(sum([x * x for x in number_list]) / len(number_list))
+    return sqrt(sum([x * x for x in number_list]) / len(number_list))
 
 
 def proportion(number_list):
@@ -78,23 +81,36 @@ parser.add_argument('-c', '--c_orthography', action='store_const',
                     default=False, const=True,
                     help='Use the c-orthography (less phonetic) instead of the'
                          ' k-orthography (more phonetic) (default: False)')
+parser.add_argument('-f', '--freqtoken', action='store_const',
+                    default=False, const=True,
+                    help='Use token frequencies in lexicon '
+                         '(default: False)')
+parser.add_argument('-l', '--latex', action='store_const',
+                    default=False, const=True,
+                    help='Compile the output LaTeX file (default: False)')
+parser.add_argument('-r', '--run_r_script', action='store_const',
+                    default=False, const=True,
+                    help='Run R script (default: False)')
 
 args = parser.parse_args()
 use_tech_terms = args.tech
 use_c_orthography = args.c_orthography
 treat_a_as_truncated_stem = args.a_as_TS
+use_token_frequency = args.freqtoken
+compile_latex = args.latex
+run_r_script = args.run_r_script
 
 # -----------------------------------------------------------------------------#
 # make sure the directories for output files are present
 
-word_plots_dir = 'plots_words'
-error_plots_dir = 'plots_errors'
+results_dir = 'results'  # directory for plots, CSV, etc.
+word_plots_dir = 'plots_for_words'
 
 if not os.path.isdir(word_plots_dir):
     os.makedirs(word_plots_dir)
 
-if not os.path.isdir(error_plots_dir):
-    os.makedirs(error_plots_dir)
+if not os.path.isdir(results_dir):
+    os.makedirs(results_dir)
 
 # -----------------------------------------------------------------------------#
 # determine file suffix
@@ -103,36 +119,51 @@ file_suffix = ''
 
 if treat_a_as_truncated_stem:
     file_suffix += '-a'
-if not use_tech_terms:
-    file_suffix += '-notech'
+if use_tech_terms:
+    file_suffix += '-tech'
 if use_c_orthography:
     file_suffix += '-c'
+if use_token_frequency:
+    file_suffix += '-tokenfreq'
+
+goldstandard_file_suffix = file_suffix.replace('-tokenfreq', '')
+
+if treat_a_as_truncated_stem:
+    file_suffix = '-a-tech'
+    goldstandard_file_suffix = '-a-tech'
+    print('NOTE: -a can be active only when -t (but nothing else) is as well.')
 
 # -----------------------------------------------------------------------------#
 # read lexicon
 
-lexiconfile = 'pt_br_orthofix'
+lexicon_file = 'pt_br_orthofix'
+
 if use_c_orthography:
-    lexiconfile += '-c.txt'
+    lexicon_file += '-c.txt'
 else:
-    lexiconfile += '.txt'
+    lexicon_file += '.txt'
 
 lex_freq_dict = dict()
 
-for line in open(os.path.join('data', lexiconfile)).readlines():
+for line in open(os.path.join('data', lexicon_file)).readlines():
     line = line.strip()
     word, freq = line.split()
     lex_freq_dict[word] = int(freq)
 
 lex_keys = lex_freq_dict.keys()
 
+lex_log_freq_dict = dict()
+
+for word in lex_keys:
+    lex_log_freq_dict[word] = log(lex_freq_dict[word], 10)
+
 # -----------------------------------------------------------------------------#
-#    read gold standard words
+# read gold standard words
 
 goldstandard_binRL_filename = os.path.join(
-    'data', 'goldStandard_binRL_orthofix%s.txt' % file_suffix)
+    'data', 'goldStandard_binRL_orthofix%s.txt' % goldstandard_file_suffix)
 goldstandard_binLR_filename = os.path.join(
-    'data', 'goldStandard_binLR_orthofix%s.txt' % file_suffix)
+    'data', 'goldStandard_binLR_orthofix%s.txt' % goldstandard_file_suffix)
 
 goldstandard_binRL_annotated = [x.strip().split("\t")[0].replace('#', '')
                                 for x in open(goldstandard_binRL_filename)]
@@ -143,8 +174,6 @@ test_words = [x.replace('|', '').replace('$', '')
               for x in goldstandard_binRL_annotated]
 
 goldstandard_list = [x.replace('$', '') for x in goldstandard_binRL_annotated]
-
-
 
 goldstandard_binRL_list = [x.replace('|', '')
                            for x in goldstandard_binRL_annotated]
@@ -163,7 +192,8 @@ binLR_trunc_points = [x.index("$") for x in goldstandard_binLR_list]
 # (right-completes = previously "successor frequencies = SF")
 # (left-completes = previously "predecessor frequencies = PF")
 
-print("\ncomputing right- and left-complete counts...")
+print("\nComputing right- and left-complete counts...")
+
 
 def compute_SF_PF_counts(test_word):
     print(test_word)
@@ -185,13 +215,21 @@ def compute_SF_PF_counts(test_word):
 
         for word in lex_keys:
 
+            if use_token_frequency:
+                word_weight = lex_log_freq_dict[word]
+            else:
+                word_weight = 1
+
+            if use_token_frequency and word_weight == 0:
+                word_weight = 0.1
+
             # This counts the number of R-completes given a "prefix"
             if word.startswith(trunc):
-                count_ += 1
+                count_ += word_weight
 
             # This counts the number of L-completes given a "suffix"
             if word.endswith(trunc_reversed):
-                count_reversed += 1
+                count_reversed += word_weight
 
         SF_counts.append(count_)
         PF_counts = [count_reversed] + PF_counts
@@ -220,12 +258,12 @@ for SF_counts, PF_counts in zip(SF_count_master_list, PF_count_master_list):
 
     for SF_count, PF_count in zip(SF_counts, PF_counts):
         if SF_count > 0:
-            log_SF = round(math.log(SF_count, 10), 2)
+            log_SF = round(log(SF_count, 10), 2)
         else:
             log_SF = 0
 
         if PF_count > 0:
-            log_PF = round(math.log(PF_count, 10), 2)
+            log_PF = round(log(PF_count, 10), 2)
         else:
             log_PF = 0
 
@@ -235,9 +273,8 @@ for SF_counts, PF_counts in zip(SF_count_master_list, PF_count_master_list):
     log_SF_master_list.append(log_SF_list)
     log_PF_master_list.append(log_PF_list)
 
-
 # -----------------------------------------------------------------------------#
-#    compute truncation points based on SF, PF, and SF+PF
+# compute truncation points based on SF, PF, and SF+PF
 
 SF_trunc_points = list()
 PF_trunc_points = list()
@@ -258,7 +295,8 @@ for log_SF_list, log_PF_list in zip(log_SF_master_list, log_PF_master_list):
 
 print("\nWriting LaTeX output...")
 
-out_tex_filename = 'outlatex%s.tex' % file_suffix
+out_tex_filename = os.path.join(results_dir,
+                                'individual_word_details%s.tex' % (file_suffix))
 out_tex = open(out_tex_filename, 'w')
 out_tex.write('\\documentclass{article}\n')
 out_tex.write('\\usepackage{booktabs}\n')
@@ -319,12 +357,12 @@ for i, word in enumerate(test_words):
 out_tex.write("\\end{document}\n")
 out_tex.close()
 
-#------------------------------------------------------------------------------#
+# ---------------------------------------------------------------------------- #
 # write R script for individual words' plots
 
 print("\nWriting R script for plotting individual words...")
 
-Rscriptname = 'plot_words.R'
+Rscriptname = os.path.join(results_dir, 'plot_words.R')
 Rscript = open(Rscriptname, 'w')
 
 for i, test_word in enumerate(test_words):
@@ -364,10 +402,34 @@ for i, test_word in enumerate(test_words):
 
 Rscript.close()
 
-#------------------------------------------------------------------------------#
-# evaluation
+# -----------------------------------------------------------------------------#
+# compile latex file and run R script
 
-print("\nEvaluation...")
+devnull = open(os.devnull, 'w')
+
+if compile_latex:
+    print("\nCompiling LaTeX file...")
+
+    try:
+        subprocess.call(('xelatex', '-output-directory=%s' % results_dir,
+                         out_tex_filename),
+                        stdout=devnull, stderr=subprocess.STDOUT)
+    except (OSError, FileNotFoundError):
+        print('The command "xelatex" is unavailable. No LaTeX compilation.')
+
+if run_r_script:
+    print("\nRunning R scripts...")
+
+    try:
+        subprocess.call(('Rscript', Rscriptname),
+                        stdout=devnull, stderr=subprocess.STDOUT)
+    except (OSError, FileNotFoundError):
+        print('The command "Rscript" is unavailable. No R scripts are run.')
+
+# ---------------------------------------------------------------------------- #
+# CSV file for errors
+
+print("\nComputing errors...")
 
 SF_eval_list = list()
 PF_eval_list = list()
@@ -390,202 +452,94 @@ for T, SF, PF, SFPF, binRL, binLR in zip(true_trunc_points, SF_trunc_points,
     binRL_eval_list.append(binRL_eval)
     binLR_eval_list.append(binLR_eval)
 
+output_csv_filename = os.path.join(results_dir, 'errors%s.csv' % file_suffix)
 
-output = open('output%s.csv' % file_suffix, 'w')
-output.write('{0},,{1},{2},{3},{4},{5}\n'.format('word', 'RC', 'LC', 'RCLC',
-                                                'BinRL', 'BinLR'))
+with open(output_csv_filename, 'w') as output:
+    output.write('{0},{1},{2},{3},{4},{5}\n'.format('word', 'RC', 'LC', 'RCLC',
+                                                    'BinRL', 'BinLR'))
 
-for gold, SF_eval, PF_eval, SFPF_eval, binRL_eval, binLR_eval in \
-    zip(goldstandard_list, SF_eval_list, PF_eval_list, SFPF_eval_list,
-        binRL_eval_list, binLR_eval_list):
+    for gold, SF_eval, PF_eval, SFPF_eval, binRL_eval, binLR_eval in \
+        zip(goldstandard_list, SF_eval_list, PF_eval_list, SFPF_eval_list,
+            binRL_eval_list, binLR_eval_list):
 
-    output.write('{0},,{1},{2},{3},{4},{5}\n'.format(gold,
-        SF_eval, PF_eval, SFPF_eval, binRL_eval, binLR_eval))
+        output.write('{0},{1},{2},{3},{4},{5}\n'.format(gold,
+            SF_eval, PF_eval, SFPF_eval, binRL_eval, binLR_eval))
 
-output.write(',sum ->,{0},{1},{2},{3},{4}\n'.format(
+# ---------------------------------------------------------------------------- #
+# Writing evaluation file
+
+print("\nEvaluating results...")
+
+stats_results_filename = os.path.join(results_dir,
+                                      'evaluation%s.txt' % file_suffix)
+stats_results_file = open(stats_results_filename, 'w')
+
+row_template = '{:<20}{:<15}{:<15}{:<15}{:<15}{:<15}\n'  # <20 to left-align
+row_float_template = '{:<20}{:<15.2f}{:<15.2f}{:<15.2f}{:<15.2f}{:<15.2f}\n'
+
+stats_results_file.write(row_template.format('', 'RC', 'LC', 'RCLC',
+                                             'BinRL', 'BinLR'))
+
+stats_results_file.write(row_float_template.format('sum',
     sum(SF_eval_list), sum(PF_eval_list), sum(SFPF_eval_list),
     sum(binRL_eval_list), sum(binLR_eval_list)))
 
-output.write(',abs values ->,{0},{1},{2},{3},{4}\n'.format(
+stats_results_file.write(row_float_template.format('abs values',
     sum_abs(SF_eval_list), sum_abs(PF_eval_list), sum_abs(SFPF_eval_list),
     sum_abs(binRL_eval_list), sum_abs(binLR_eval_list)))
 
-output.write(',RMS ->,{0},{1},{2},{3},{4}\n'.format(
+stats_results_file.write(row_float_template.format('RMS',
     rms(SF_eval_list), rms(PF_eval_list), rms(SFPF_eval_list),
     rms(binRL_eval_list), rms(binLR_eval_list)))
 
-output.write(',correct proportion,{0},{1},{2},{3},{4}\n'.format(
+stats_results_file.write(row_float_template.format('correct proportion',
     proportion(SF_eval_list), proportion(PF_eval_list),
     proportion(SFPF_eval_list),
     proportion(binRL_eval_list), proportion(binLR_eval_list)))
 
-output.write(',mean,{0},{1},{2},{3},{4}\n'.format(
+stats_results_file.write(row_float_template.format('mean',
     np.mean(SF_eval_list), np.mean(PF_eval_list), np.mean(SFPF_eval_list),
     np.mean(binRL_eval_list), np.mean(binLR_eval_list)))
 
-output.write(',std dev,{0},{1},{2},{3},{4}\n'.format(
+stats_results_file.write(row_float_template.format('std dev',
     np.std(SF_eval_list), np.std(PF_eval_list), np.std(SFPF_eval_list),
     np.std(binRL_eval_list), np.std(binLR_eval_list)))
-
-output.close()
 
 anova = stats.f_oneway(SF_eval_list, PF_eval_list, SFPF_eval_list,
                        binRL_eval_list, binLR_eval_list)
 
-print('\nOne-way ANOVA:\nF-value: {}\np-value: {}'.format(*anova))
+print('\nOne-way ANOVA:\nF-value: {}\np-value: {}'.format(*anova),
+      file=stats_results_file)
 
-print('\nKolmogorov-Smirnov tests')
+print('\nKolmogorov-Smirnov tests', file=stats_results_file)
 
 for data, model in zip(
         [SF_eval_list, PF_eval_list, binRL_eval_list, binLR_eval_list],
-        ['SF', 'PF', 'binRL', 'binLR']):
+        ['RC', 'LC', 'binRL', 'binLR']):
     ks = stats.ks_2samp(SFPF_eval_list, data)
-    print('Models: SFPF and {} |'.format(model), end=' ')
-    print('KS statistic: {} p-value: {}'.format(*ks))
+    print('Models: RCLC and {} |'.format(model), end=' ',
+          file=stats_results_file)
+    print('KS statistic: {:.4f} p-value: {}'.format(*ks),
+          file=stats_results_file)
 
-############################################################
-# compile latex file and run R script
+stats_results_file.close()
 
-devnull = open(os.devnull, 'w')
+# -----------------------------------------------------------------------------#
+# boxplot
 
-print("\nCompiling LaTeX file...")
-subprocess.call(('xelatex', out_tex_filename),
-                stdout=devnull, stderr=subprocess.STDOUT)
+models = ['RC', 'LC', 'RCLC', 'BinRL', 'BinLR']
+eval_data = [SF_eval_list, PF_eval_list, SFPF_eval_list,
+             binRL_eval_list, binLR_eval_list]
 
-print("\nRunning R scripts...")
-subprocess.call(('Rscript', Rscriptname),
-                stdout=devnull, stderr=subprocess.STDOUT)
+boxplot_data = pd.DataFrame({model: data
+                             for model, data in zip(models, eval_data)})
 
-#------------------------------------------------------------------------------#
-#    write and run R script for error distribution plots
+boxplot = sns.boxplot(data=boxplot_data)
+boxplot.set(ylabel='Error', ylim=(-5, 9))
+boxplot.get_figure().savefig(os.path.join(results_dir,
+                                          'error_distribution_boxplot%s.pdf'
+                                          % (file_suffix)))
 
-print("\nWriting R script for plotting error distributions...")
-
-scriptstring = '''
-# plot density curves of error distributions
-
-data = read.csv('output%s.csv', header= TRUE)
-data = subset(data, word != "")
-
-linewidth = 2 # default is 1
-density.adjust = 1.5 # default is 1
-
-data.RC = data$RC
-data.LC = data$LC
-data.RCLC = data$RCLC
-data.BinaryFootRL = data$BinRL
-data.BinaryFootLR = data$BinLR
-
-minX = min(c(data.RC, data.LC, data.RCLC, data.BinaryFootRL, data.BinaryFootLR))
-maxX = max(c(data.RC, data.LC, data.RCLC, data.BinaryFootRL, data.BinaryFootLR))
-
-minY = min(as.data.frame(table(data.RC))[2], as.data.frame(table(data.LC))[2], 
-           as.data.frame(table(data.RCLC))[2],
-           as.data.frame(table(data.BinaryFootRL))[2],
-           as.data.frame(table(data.BinaryFootLR))[2])
-
-maxY = max(as.data.frame(table(data.RC))[2], as.data.frame(table(data.LC))[2], 
-           as.data.frame(table(data.RCLC))[2],
-           as.data.frame(table(data.BinaryFootRL))[2],
-           as.data.frame(table(data.BinaryFootLR))[2])
-
-RC.density = density(data.RC, adjust=density.adjust)
-LC.density = density(data.LC, adjust=density.adjust)
-RCLC.density = density(data.RCLC, adjust=density.adjust)
-binftRL.density = density(data.BinaryFootRL, adjust=density.adjust)
-binftLR.density = density(data.BinaryFootLR, adjust=density.adjust)
-
-minX.density = min(c(RC.density$x, LC.density$x, RCLC.density$x,
-                     binftRL.density$x, binftLR.density$x))
-maxX.density = max(c(RC.density$x, LC.density$x, RCLC.density$x,
-                     binftRL.density$x, binftLR.density$x))
-minY.density = min(c(RC.density$y, LC.density$y, RCLC.density$y,
-                     binftRL.density$y, binftLR.density$y))
-maxY.density = max(c(RC.density$y, LC.density$y, RCLC.density$y,
-                     binftRL.density$y, binftLR.density$y))
-
-# plot for paper
-postscript('%s/error-distribution-density%s.eps')
-plot(RC.density, lty=2, xlim=c(minX.density, maxX.density),
-    ylim=c(minY.density, maxY.density), lwd=linewidth,
-    main="", ylab="Density", xlab="Error")
-lines(LC.density, lty=3, lwd=linewidth, ann=FALSE)
-lines(RCLC.density, lty=1, lwd=linewidth, ann=FALSE)
-lines(binftRL.density, lty=4, lwd=linewidth, ann=FALSE)
-lines(binftLR.density, lty=5, lwd=linewidth, ann=FALSE)
-legend(minX.density+1, maxY.density-0.05,
-    c("RC","LC", "RC+LC", "BinRL", "BinLR"), lty=c(2,3,1,4,5), lwd=linewidth)
-dev.off()
-
-# colored plot (using density)
-postscript('%s/error-distribution-density-colored%s.eps')
-plot(RC.density, lty=1, xlim=c(minX.density, maxX.density),
-    ylim=c(minY.density, maxY.density), lwd=linewidth,
-    main="Error distributions", ylab="Density", xlab="Error")
-lines(LC.density, lty=2, lwd=linewidth, ann=FALSE)
-lines(RCLC.density, lty=1, lwd=3, col="#800000", ann=FALSE) # color is maroon
-lines(binftRL.density, lty=1, lwd=linewidth, col="#0000FF", ann=FALSE) # blue
-lines(binftLR.density, lty=2, lwd=linewidth, col="#0000FF", ann=FALSE) # blue
-legend(minX.density+1, maxY.density-0.05, c("RC","LC","RC+LC","BinRL","BinLR"),
-    lty=c(1,2,1,1,2), lwd=linewidth,
-    text.col=c("#000000", "#000000", "#800000", "#0000FF", "#0000FF"),
-    col=c("#000000", "#000000", "#800000", "#0000FF", "#0000FF"))
-dev.off()
-
-
-###################
-# plot individual histograms
-
-fillcolor = 'gray'
-ylabname = 'Count'
-maxY.overall = max(maxY, maxY.density*100)
-
-postscript('%s/error-distribution-histogram-RC%s.eps')
-hist(data.RC, xlim=c(minX, maxX), ylim=c(minY, maxY.overall),
-    col=fillcolor, ylab=ylabname)
-lines(x=RC.density$x, y=RC.density$y*100, lwd=linewidth)
-dev.off()
-
-postscript('%s/error-distribution-histogram-LC%s.eps')
-hist(data.LC, xlim=c(minX, maxX), ylim=c(minY, maxY.overall),
-    col=fillcolor, ylab=ylabname)
-lines(x=LC.density$x, y=LC.density$y*100, lwd=linewidth)
-dev.off()
-
-postscript('%s/error-distribution-histogram-RCLC%s.eps')
-hist(data.RCLC, xlim=c(minX, maxX), ylim=c(minY, maxY.overall),
-    col=fillcolor, ylab=ylabname)
-lines(x=RCLC.density$x, y=RCLC.density$y*100, lwd=linewidth)
-dev.off()
-
-postscript('%s/error-distribution-histogram-BinFtRL%s.eps')
-hist(data.BinaryFootRL, xlim=c(minX, maxX), ylim=c(minY, maxY.overall),
-    col=fillcolor, ylab=ylabname)
-lines(x=binftRL.density$x, y=binftRL.density$y*100, lwd=linewidth)
-dev.off()
-
-postscript('%s/error-distribution-histogram-BinFtLR%s.eps')
-hist(data.BinaryFootLR, xlim=c(minX, maxX), ylim=c(minY, maxY.overall),
-    col=fillcolor, ylab=ylabname)
-lines(x=binftLR.density$x, y=binftLR.density$y*100, lwd=linewidth)
-dev.off()
-
-''' % (file_suffix,
-       error_plots_dir, file_suffix,
-       error_plots_dir, file_suffix,
-       error_plots_dir, file_suffix,
-       error_plots_dir, file_suffix,
-       error_plots_dir, file_suffix,
-       error_plots_dir, file_suffix,
-       error_plots_dir, file_suffix,
-       )
-
-errorscriptname = 'plot_error_distributions.R'
-
-with open(errorscriptname, 'w') as Rscriptfile:
-    Rscriptfile.write(scriptstring)
-
-subprocess.call(('Rscript', errorscriptname),
-                stdout=devnull, stderr=subprocess.STDOUT)
+# -----------------------------------------------------------------------------#
 
 print("\nAll done!")
